7dda9643ae607e70d67f8edaa86af607071530d2
Serhii Temchenko <serg.temchenko@gmail.com>
2019-09-20 14:01:10 +0300

    Extended validation listener with ability to chose submission method

--- EventListener/AngularCsrfValidationListener.php
+++ EventListener/AngularCsrfValidationListener.php
@@ -11,6 +11,7 @@ namespace Dunglas\AngularCsrfBundle\EventListener;
 
 use Dunglas\AngularCsrfBundle\Csrf\AngularCsrfTokenManager;
 use Dunglas\AngularCsrfBundle\Routing\RouteMatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
@@ -39,6 +40,14 @@ class AngularCsrfValidationListener
      */
     protected $headerName;
     /**
+     * @var string
+     */
+    private $tokenSubmitMethod;
+    /**
+     * @var string
+     */
+    private $tokenName;
+    /**
      * @var array
      */
     private $exclude;
@@ -48,6 +57,8 @@ class AngularCsrfValidationListener
      * @param RouteMatcherInterface   $routeMatcher
      * @param array                   $routes
      * @param string                  $headerName
+     * @param string                  $tokenSubmitMethod
+     * @param string                  $tokenName
      * @param array                   $exclude
      */
     public function __construct(
@@ -55,13 +66,17 @@ class AngularCsrfValidationListener
         RouteMatcherInterface $routeMatcher,
         array $routes,
         $headerName,
+        string $tokenSubmitMethod,
+        string $tokenName,
         array $exclude = array()
     ) {
         $this->angularCsrfTokenManager = $angularCsrfTokenManager;
         $this->routeMatcher = $routeMatcher;
         $this->routes = $routes;
-        $this->exclude = $exclude;
         $this->headerName = $headerName;
+        $this->exclude = $exclude;
+        $this->tokenSubmitMethod = $tokenSubmitMethod;
+        $this->tokenName = $tokenName;
     }
 
     /**
@@ -83,9 +98,37 @@ class AngularCsrfValidationListener
             return;
         }
 
-        $value = $event->getRequest()->headers->get($this->headerName);
+        $value = $this->getToken($event->getRequest());
+
         if (!$value || !$this->angularCsrfTokenManager->isTokenValid($value)) {
             throw new AccessDeniedHttpException('Bad CSRF token.');
         }
+
+        $this->unsetQueryStringParameter($event->getRequest());
+    }
+
+    /**
+     * @param Request $request
+     * @return string|null
+     */
+    private function getToken(Request $request)
+    {
+        if ('query_string' === $this->tokenSubmitMethod) {
+            return $request->query->get($this->tokenName);
+        }
+
+        if ('header' === $this->tokenSubmitMethod) {
+            return $request->headers->get($this->headerName);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     */
+    private function unsetQueryStringParameter(Request $request)
+    {
+        $request->query->remove($this->tokenName);
     }
 }
